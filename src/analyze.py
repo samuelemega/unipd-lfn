39f1731c25ff15ab29301c73d458c7629f5a0fe5
CognoXD <64655738+CognoXD@users.noreply.github.com>
2025-01-15 17:30:49 +0100

    polished code

--- src/analyze.py
+++ src/analyze.py
@@ -1,30 +1,21 @@
+"""
+  Author: Federico Cognolatto
+"""
+
+import time
+
 import igraph as ig
 
+from intra_cluster import communities_avg_intra_cluster_distance
+
 if __name__ == "__main__":
-    print("starting graph analysis")
-    g=ig.Graph.Read_Pickle("src\\files\\graph.pkl")
-    ug= g.as_undirected()
-    print("graph created")
-    com=ig.Graph.community_multilevel(ug)
-    print("clustering completed")
-    nNodes=ug.vcount()
-    subcoms=com.subgraphs()
-    print("subgraphs created")
-    with open("src\\files\\res.txt", "w") as f:
-      i=0
-      print("computing avg distance")
-      d=ug.average_path_length(directed=False)
-      print("avg intra-cluster distance:", repr(d),file=f)
-      for sub in subcoms:
-        print("counting nodes")
-        sNodes=sub.vcount()
-        sRatio=sNodes/nNodes*100
-        print("dimension of cluster",repr(i),":", repr(sRatio),file=f)
-        #d+=sub.average_path_length(directed=False)
-        i+=1
-      #print("avg intra-cluster distance:", repr(d/i),file=f)
-    with open("src\\files\\res.txt","w") as f:
-      print("louvain modularity score:",repr(com.modularity),file=f)
-    #ig.plot(ug,"prova.pdf")
-    
-      
+  g=ig.Graph.Read_Pickle("src\\files\\graph.pkl")
+  ug= g.as_undirected()
+  startTime=time.time()
+  com=ig.Graph.community_multilevel(ug)
+  endTime=time.time()-startTime
+  with open("src\\files\\res.txt", "w") as f:
+    print("louvain time execution in seconds:",repr(endTime),file=f)
+    print("louvain modularity score:",repr(com.modularity),file=f)
+  communities=com.subgraphs()
+  communities_avg_intra_cluster_distance(communities)
